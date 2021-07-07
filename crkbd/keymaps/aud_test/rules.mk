16181185dce772fdb047a7cf15c4ef56f8d4d650
Mondo12 <AudreyChoi12@gmail.com>
2021-07-07 13:51:04 -0700

    Delete crkbd directory

--- crkbd/keymaps/aud_test/rules.mk
+++
@@ -1,8 +0,0 @@
-
-RGB_MATRIX_ENABLE = yes
-BOOTLOADER = atmel-dfu # elite-c
-
-POINTING_DEVICE_ENABLE = yes
-SRC += pimoroni_trackball.c
-QUANTUM_LIB_SRC += i2c_master.c
-MOUSEKEY_ENABLE = no
--- crkbd/rev1/legacy/config.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "serial_config.h"
